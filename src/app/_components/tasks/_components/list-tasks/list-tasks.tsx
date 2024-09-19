aaba575eb47971846209018e256c60302836891b
Pablo Alves <ribeirojr1522@gmail.com>
2024-09-19 15:20:02 -0300

    refactor: :recycle: change task status and add through task completed

--- src/app/_components/tasks/_components/list-tasks/list-tasks.tsx
+++ src/app/_components/tasks/_components/list-tasks/list-tasks.tsx
@@ -1,20 +1,22 @@
 'use client'
 import styles from './styles.module.scss'
 
-import { TaskProps } from '../../tasks'
 import { LuTrash } from 'react-icons/lu'
 import { Checkbox } from '@/components/ui'
+import { StatusTaskProps, TaskProps, UpdateTaskStatusProps } from '@/store'
 
 interface ListTasksProps {
   title: string
   tasks: TaskProps[]
   deleteTask: (taskId: number) => void
-  changeStatusTask: (taskId: number) => void
+  changeStatusTask: (data: UpdateTaskStatusProps) => void
+  status: StatusTaskProps
 }
 
 export function ListTasks({
   title,
   tasks,
+  status,
   changeStatusTask,
   deleteTask,
 }: ListTasksProps) {
@@ -22,8 +24,13 @@ export function ListTasks({
     <div className={styles.container}>
       <h2>{title}</h2>
       {tasks.length === 0 && (
-        <div className={styles.card}>
-          <p>Você não possui tarefas, adicione novas tarefas!</p>
+        <div className={styles.description}>
+          {status === 'completed' && (
+            <p>Você não possui tarefas finalizadas!</p>
+          )}
+          {status === 'pending' && (
+            <p>Você não possui tarefas, adicione novas tarefas!</p>
+          )}
         </div>
       )}
       {tasks.length > 0 && (
@@ -32,10 +39,20 @@ export function ListTasks({
             <li className={styles.card} key={task.id}>
               <div className={styles.card_content}>
                 <Checkbox
-                  onClick={() => changeStatusTask(task.id)}
+                  onClick={() =>
+                    changeStatusTask({
+                      status:
+                        task.status === 'completed' ? 'pending' : 'completed',
+                      taskId: task.id,
+                    })
+                  }
                   checked={task.status === 'completed'}
                 />
-                <p>{task?.title}</p>
+                <p
+                  className={`${styles.description} ${task.status === 'completed' && styles.completed}`}
+                >
+                  {task?.title}
+                </p>
               </div>
               <button
                 onClick={() => deleteTask(task.id)}
