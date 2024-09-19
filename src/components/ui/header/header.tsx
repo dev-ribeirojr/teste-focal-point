e7da6dda17680e036d48a74075a89c88ec8f5f99
Pablo Alves <ribeirojr1522@gmail.com>
2024-09-19 11:54:10 -0300

    styles: :recycle: align title header

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -5,12 +5,14 @@ export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
-        <Image
-          src="/logo.svg"
-          alt="Logo marca da Focal Point"
-          height={36}
-          width={150}
-        />
+        <div className={styles.content_logo}>
+          <Image
+            src="/logo.svg"
+            alt="Logo marca da Focal Point"
+            height={36}
+            width={150}
+          />
+        </div>
         <h1>Bem-vindo de volta, Marcus</h1>
         <p>Segunda, 01 de dezembro de 2025</p>
       </div>
